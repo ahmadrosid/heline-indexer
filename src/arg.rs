e71a5da6b17f1d93928a28f48bee20e4e501ce5e
ahmadrosid <alahmadrosid@gmail.com>
2022-08-17 11:56:01 +0700

    feat: add help command

--- src/arg.rs
+++ src/arg.rs
@@ -18,40 +18,69 @@ impl Arg {
         }
     }
 
+    // Parse to: hli index_file.json --folder some/path
     pub fn parse(mut self) -> Result<Self, String> {
         self.solr_url = match env::var("BASE_URL") {
             Ok(val) => val,
             Err(_) => "http://localhost:8984".to_string(),
         };
 
-        let file = match env::args().nth(1) {
+        let arg_input: Vec<String> = env::args().collect();
+
+        for input in &arg_input {
+            if input == "-h" || input == "--help" {
+                self.print_help();
+                std::process::exit(0);
+            }
+        }
+
+        let file = match arg_input.get(1) {
             Some(file) => file,
             None => {
                 return Err(format!("Index file path is required!"));
             }
         };
 
-        if !Path::new(&file).exists() {
+        if !Path::new(file).exists() {
             return Err(format!(
                 "Please input the correct index file! Current file is not exists: {}",
-                &file
+                file
             ));
         }
 
-        let option = match std::env::args().nth(2) {
+        let option = match arg_input.get(2) {
             Some(option) => option,
-            None => String::new(),
+            None => "",
         };
 
         self.is_index_folder = option == "--folder";
         let mut repo_folder = PathBuf::new();
-        match env::args().nth(3) {
-            Some(folder) => repo_folder.push(&folder),
+        match arg_input.get(3) {
+            Some(folder) => repo_folder.push(folder),
             None => repo_folder.push("repos"),
         };
 
         self.folder = repo_folder;
 
         Ok(self)
+    }
+
+    fn print_help(&self) {
+        let help_text = vec![
+            "hli 0.1.0",
+            "Ahmad Rosid <alahmadrosid@gmail.com>",
+            "",
+            "Heline.dev indexer, turn source code to github like html syntax highlighted!",
+            "",
+            "Usage :",
+            "    hli data.json --folder some/folder",
+            "",
+            "Options :",
+            "    <INDEX_FILE>    Index file with json format",
+            "    --folder        Custom folder to source code",
+            "    -h --help       Print help text",
+            ""
+        ];
+        println!("{}", help_text.join("\n"));
     }
 }
